c70afd1d98a0436dba7f52fd7ef3b58bbcda3e00
Zig Zichterman <github@ziggr.com>
2018-11-28 20:53:09 -0800

    /example output screenshot
    And omit those giant data point history lists from MM dumps.

--- LibPrice_Example.lua
+++ LibPrice_Example.lua
@@ -2,27 +2,37 @@ LibPrice_Example = LibPrice_Example or {}
 
 SLASH_COMMANDS["/example"] = function() LibPrice_Example.SlashCommand() end
 
+local EXAMPLE_ITEMS = {
+                    -- essence of health
+      "|H1:item:54339:308:50:0:0:0:0:0:0:0:0:0:0:0:0:36:1:0:0:0:65536|h|h"
+                    -- Crown Tri-Restoration Potion
+    , "|H1:item:64710:123:1:0:0:0:0:0:0:0:0:0:0:0:1:36:0:1:0:0:0|h|h"
+                    -- Necklace of Willpower
+    , "|H1:item:69278:363:50:0:0:0:0:0:0:0:0:0:0:0:257:24:0:1:0:0:0|h|h"
+                    -- Trees, Paired Evergreens
+    , "|H1:item:120550:3:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"
+                    -- Varla Stone
+    , "|H1:item:134465:5:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"
+                    -- Recipe: Raven Rock Baked Ash Yams
+    , "|H0:item:56970:4:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"
+                    -- Platinum Necklace, intricate
+    , "|H0:item:138797:307:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"
+                    -- Jewelry station Naga Shaman
+    , "|H1:item:145501:124:1:0:0:0:0:0:0:0:0:0:0:0:1:0:0:1:0:0:0|h|h"
+                    -- Mundus Stone: The Warrior
+    , "|H1:item:125453:6:1:0:0:0:0:0:0:0:0:0:0:0:65:0:0:1:0:0:0|h|h"
+                    -- Transmute Station
+    , "|H1:item:133576:6:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"
+    }
 
 function LibPrice_Example.SlashCommand()
-    local ITEMS = {
-          "|H1:item:54339:308:50:0:0:0:0:0:0:0:0:0:0:0:0:36:1:0:0:0:65536|h|h"  -- essence of health
-        , "|H1:item:64710:123:1:0:0:0:0:0:0:0:0:0:0:0:1:36:0:1:0:0:0|h|h"       -- Crown Tri-Restoration Potion
-        , "|H1:item:69278:363:50:0:0:0:0:0:0:0:0:0:0:0:257:24:0:1:0:0:0|h|h"    -- Necklace of Willpower
-        , "|H1:item:120550:3:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"         -- Trees, Paired Evergreens
-        , "|H1:item:134465:5:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"         -- Varla Stone
-        , "|H0:item:56970:4:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"          -- Recipe: Raven Rock Baked Ash Yams
-        , "|H0:item:138797:307:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"      -- Platinum Necklace, intricate
-        , "|H1:item:145501:124:1:0:0:0:0:0:0:0:0:0:0:0:1:0:0:1:0:0:0|h|h"       -- Jewelry station Naga Shaman
-        , "|H1:item:125453:6:1:0:0:0:0:0:0:0:0:0:0:0:65:0:0:1:0:0:0|h|h"        -- Mundus Stone: The Warrior
-        , "|H1:item:133576:6:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"         -- Transmute Station
-
-        }
-    for _,item_link in ipairs(ITEMS) do
+    for _,item_link in ipairs(EXAMPLE_ITEMS) do
         local result = LibPrice.LinkToPrice(item_link)
         d(item_link)
         LibPrice_Example.DumpTable(result)
     end
 end
+
                         -- A recursive table dumper that does a marginally better
                         -- job of indenting than d()
 function LibPrice_Example.DumpTable(t, indent_ct)
@@ -42,8 +52,10 @@ function LibPrice_Example.DumpTable(t, indent_ct)
     end
                         -- Can we squish down to single line?
     local has_sub_tables = false
-    for _,v in pairs(t) do
-        if type(v) == "table" then
+    for k,v in pairs(t) do
+        if type(v) == "table"
+            and k ~= "points"   -- Omit MM's huge "points" history table.
+            then
             has_sub_tables = true
             break
         end
@@ -53,6 +65,11 @@ function LibPrice_Example.DumpTable(t, indent_ct)
         local sorted = LibPrice_Example.SortedKeys(t)
         for _,k in ipairs(sorted) do
             local v = t[k]
+            if type(v) == "table" then
+                        -- omit hex addr from tables that we choose not to
+                        -- recurse into.
+                v="table"
+            end
             line = line .. string.format(grey.."%s:"..white.."%s  ",tostring(k),tostring(v))
         end
         d(indent..line)
@@ -65,7 +82,9 @@ function LibPrice_Example.DumpTable(t, indent_ct)
             local vv = tostring(v)
             if type(v) == "table" then vv = "table" end -- Omit useless hex addresses.
             d(string.format(grey..indent.."%-4s: "..white.."%s", tostring(k), vv))
-            if type(v) == "table" then
+            if type(v) == "table"
+                and k ~= "points"       -- Omit MM's huge "points" history table.
+                then
                 LibPrice_Example.DumpTable(v,1+indent_ct)
             end
         end
--- readme.md
+++ readme.md
@@ -15,7 +15,38 @@ As well as some hard-coded data from
 
 ## Example Code
 
-tbd
+```lua
+function LibPrice_Example.SlashCommand()
+    local ITEMS = {
+                        -- essence of health
+          "|H1:item:54339:308:50:0:0:0:0:0:0:0:0:0:0:0:0:36:1:0:0:0:65536|h|h"
+                        -- Crown Tri-Restoration Potion
+        , "|H1:item:64710:123:1:0:0:0:0:0:0:0:0:0:0:0:1:36:0:1:0:0:0|h|h"
+                        -- Necklace of Willpower
+        , "|H1:item:69278:363:50:0:0:0:0:0:0:0:0:0:0:0:257:24:0:1:0:0:0|h|h"
+                        -- Trees, Paired Evergreens
+        , "|H1:item:120550:3:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"
+                        -- Varla Stone
+        , "|H1:item:134465:5:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"
+                        -- Recipe: Raven Rock Baked Ash Yams
+        , "|H0:item:56970:4:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"
+                        -- Platinum Necklace, intricate
+        , "|H0:item:138797:307:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"
+                        -- Jewelry station Naga Shaman
+        , "|H1:item:145501:124:1:0:0:0:0:0:0:0:0:0:0:0:1:0:0:1:0:0:0|h|h"
+                        -- Mundus Stone: The Warrior
+        , "|H1:item:125453:6:1:0:0:0:0:0:0:0:0:0:0:0:65:0:0:1:0:0:0|h|h"
+                        -- Transmute Station
+        , "|H1:item:133576:6:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h"
+        }
+    for _,item_link in ipairs(ITEMS) do
+        local result = LibPrice.LinkToPrice(item_link)
+        d(item_link)
+        LibPrice_Example.DumpTable(result)
+    end
+end
+```
+![example](doc/example.jpg)
 
 
 
